429b7fc29e051921a0e7e676caf02324df24d0a4
tom20314 <tom20314@gmail.com>
2014-10-13 14:40:20 +0800

    add_ya
    add_ya

---
+++ ya.py
@@ -0,0 +1,11 @@
+x = int(input("please enter an integer:\n"))
+if x < 0:
+	print("negative change to zero")
+elif x == 0:
+	print("Zero")
+elif x == 1:
+	print("Single")
+else:
+	print("More")
+	
+	
